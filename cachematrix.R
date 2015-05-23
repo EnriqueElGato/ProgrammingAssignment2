aaef66a4d7e73a7dd5210683d2968ca14b569c82
EnriqueElGato <wo.shi.wo@live.com>
2015-05-23 11:46:13 -0400

    Assignment 2 initial check in.

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,39 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    vInverseOfX <- NULL
+    
+    setX <- function(pMatrix){
+        x <<- pMatrix
+    }
+    
+    getX <- function() x
+    
+    setInverse <- function(pInverse) {
+        vInverseOfX <<- pInverse
+    }
+    
+    getInverse <- function() vInverseOfX
+    
+    list(setX = setX, getX = getX,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    vInverse <- x$getInverse()
+    if(!is.null(vInverse)) {
+        message("Returning cached inverse")
+        return(vInverse)
+    }
+    message("Calculating inverse")
+    vMatrix <- x$getX()
+    vInverse <- solve(vMatrix, ...)
+    x$setInverse(vInverse)
+    vInverse
+    
 }
